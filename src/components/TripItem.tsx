731d365209e3ad8154162ce2258e0671d82d25c8
Ankit Tiwari <ankitt@valuefy.com>
2022-01-08 23:35:04 +0530

    Background color change on appState

--- src/components/TripItem.tsx
+++ src/components/TripItem.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import React, {useRef, useEffect, useState} from 'react';
+import {View, Text, StyleSheet, TouchableOpacity, AppState} from 'react-native';
 import colors from '../util/colors';
 
 interface itemProps {
@@ -20,15 +20,36 @@ interface Props {
 }
 
 const TripItem = ({item, name, startDate, endDate, status, onPress}: Props) => {
+  const appState = useRef(AppState.currentState);
+  const [bgColor, setBgColor] = useState(colors.TRIP_CARD_BACKGROUND);
+
+  useEffect(() => {
+    const subscription = AppState.addEventListener('change', nextAppState => {
+      if (
+        appState.current.match(/inactive|background/) &&
+        nextAppState === 'active'
+      ) {
+        status === 'NOT_STARTED' &&
+          setBgColor(colors.TRIP_CARD_BACKGROUND_HIGHLIGHTED);
+      }
+
+      appState.current = nextAppState;
+    });
+
+    return () => {
+      subscription.remove();
+    };
+  }, []);
+
   return (
     <TouchableOpacity onPress={() => onPress(item)}>
-      <View style={styles.tripItem}>
+      <View style={[styles.tripItem, {backgroundColor: bgColor}]}>
         <Text style={styles.name}>{name}</Text>
         <View style={styles.dateRow}>
           <Text style={styles.date}>{startDate}</Text>
           <Text style={styles.date}>{endDate}</Text>
         </View>
-        <Text style={styles.status}>{status}</Text>
+        <Text style={styles.status}>{status.replace(/_/g, ' ')}</Text>
       </View>
     </TouchableOpacity>
   );
@@ -42,7 +63,7 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     padding: 15,
     marginBottom: 15,
-    backgroundColor: colors.TRIP_CARD_BACKGROUND,
+    // backgroundColor: colors.TRIP_CARD_BACKGROUND,
   },
   dateRow: {
     flexDirection: 'row',
